a656caf69573b07f08f24c26b652573cfe98637d
zackshen <zackshen0526@gmail.com>
2018-08-23 22:36:34 +0800

    stack benchmark

--- chapter1/stack/stack_test.go
+++ chapter1/stack/stack_test.go
@@ -4,6 +4,16 @@ import (
 	"testing"
 )
 
+var stackWithElements *Stack
+var StackElementCount = 100000
+
+func init() {
+	stackWithElements = NewStack()
+	for i := 0; i < StackElementCount; i++ {
+		stackWithElements.Push("A")
+	}
+}
+
 func TestStack(t *testing.T) {
 	stack := NewStack()
 	stack.Push(1)
@@ -31,4 +41,21 @@ func TestStack(t *testing.T) {
 		t.Errorf("stack is not empty")
 	}
 
+}
+
+func BenchmarkStackPush(b *testing.B) {
+	stack := NewStack()
+	for i := 0; i < b.N; i++ {
+		stack.Push("A")
+	}
+}
+
+func BenchmarkStackPop(b *testing.B) {
+	for i := 0; i < StackElementCount; i++ {
+		stackWithElements.Pop()
+	}
+}
+
+func BenchmarkStackSize(b *testing.B) {
+	stackWithElements.Size()
 }
