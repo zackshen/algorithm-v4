70869534ca07d99817f63f8e92a41669cff4e5c8
zackshen <zackshen0526@gmail.com>
2018-09-20 21:33:28 +0800

    插入排序

--- chapter2/insert-sort/insertion_sort.go
+++ chapter2/insert-sort/insertion_sort.go
@@ -5,15 +5,18 @@ func InsertSort(data []int) {
 	for i := 1; i < N; i++ {
 		value := data[i]
 		index := i
+		// 因为从左往右是从小到大的排列顺序
+		// 从后往前找比value大的数
+		// 如果比value大就往后挪一位
+		// 直到比value小就退出循环
 		for j := i; j > 0 && data[j-1] > value; j-- {
 			data[j] = data[j-1]
 			index = j - 1
 		}
+		// 将value放入最终停下的位置
 		data[index] = value
 	}
 }
-
-// 1 55 89
 
 func main() {
 
