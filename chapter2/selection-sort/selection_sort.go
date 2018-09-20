bacfbebb5d074a7f231bd11530ac0947d5d24197
zackshen <zackshen0526@gmail.com>
2018-09-20 21:36:03 +0800

    选择排序

--- chapter2/selection-sort/selection_sort.go
+++ chapter2/selection-sort/selection_sort.go
@@ -9,12 +9,14 @@ func SelectionSort(data []int) {
 	for i := 0; i < N; i++ {
 		minValue := data[i]
 		minValueIndex := i
+		// 从当前索引往后找，找到最小的一个数
 		for j := i + 1; j < N; j++ {
 			if data[j] <= minValue {
 				minValue = data[j]
 				minValueIndex = j
 			}
 		}
+		// 将从索引右侧找到的最小数与当前数交换位置
 		swap(i, minValueIndex, data)
 	}
 }
